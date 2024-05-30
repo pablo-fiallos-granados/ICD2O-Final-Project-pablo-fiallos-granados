7eb06d8bde1934db11dc57ff9fffb763177713e1
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-05-30 17:48:30 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Pablo Fiallos
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function updateCookieCount () {
+
+  if (localStorage.cookies) {
+    document.getElementById("user-info").innerHTML = "Cookies: " + localStorage.cookies
+  } else {
+    document.getElementById("user-info").innerHTML = "Cookies: 0"
+  }
+}
+
+function cookieClicked() {
+  if (localStorage.cookies) {
+    localStorage.cookies = Number(localStorage.cookies) + 1 
+  } else {
+    localStorage.cookies = 1
+  }
+  document.getElementById("user-info").innerHTML = "Cookies: " + localStorage.cookies
+}
